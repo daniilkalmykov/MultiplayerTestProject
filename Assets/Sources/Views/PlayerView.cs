c3952f9185f28e5fc133611816f5c23e4a84fedf
daniilkalmykov <daniilk24960@gmail.com>
2023-09-25 20:40:48 +0600

    add player view

---
+++ Assets/Sources/Views/PlayerView.cs
@@ -0,0 +1,38 @@
+using System;
+using Sources.InputSystem;
+using Sources.MovementSystem;
+using UnityEngine;
+
+namespace Sources.Views
+{
+    internal sealed class PlayerView : MonoBehaviour
+    {
+        private IMovable _movable;
+        private IInput _input;
+        private Rigidbody _rigidbody;
+
+        private void Update()
+        {
+            if (_input == null || _movable == null)
+                return;
+
+            Move();
+        }
+
+        public void Init(IMovable movable, IInput input, Rigidbody rigidbody)
+        {
+            _movable = movable ?? throw new ArgumentNullException();
+            _input = input ?? throw new ArgumentNullException();
+            _rigidbody = rigidbody;
+        }
+        
+        private void Move()
+        {
+            var cameraRelativeMovement = transform.TransformDirection(_input.Horizontal, 0, _input.Vertical);
+            var direction = new Vector3(_movable.Speed * cameraRelativeMovement.x, _rigidbody.velocity.y,
+                _movable.Speed * cameraRelativeMovement.z);
+
+            _rigidbody.velocity = direction;
+        }
+    }
+}
